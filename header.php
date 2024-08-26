e02a9c7f12abb4568826fc08d5087003ef5a2293
AbdulMoiz36 <abdulmoiz8236@gmail.com>
2024-08-26 02:34:47 -0700

    Added Font Awesome CDN in header

--- header.php
+++ header.php
@@ -38,6 +38,9 @@ $totalProduct=$obj->totalProduct();
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="css/form.css">
 
+    <!-- Font Awesome 5.15.4 -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+
 
 </head>
 
